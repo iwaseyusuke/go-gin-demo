c8d64341c59aa713fbe8230da794f064b9110ad2
Yusuke Iwase <iwase.yusuke0@gmail.com>
2023-02-24 23:12:39 +0900

    hello: Use embed for templates
    Signed-off-by: Yusuke Iwase <iwase.yusuke0@gmail.com>

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"embed"
+	"html/template"
+
 	"github.com/iwaseyusuke/go-gin-demo/pkg/hello"
 
 	"github.com/gin-gonic/gin"
 )
 
+//go:embed web/templates/*.html
+var f embed.FS
+
 func main() {
 	r := gin.Default()
+	t := template.Must(template.New("").ParseFS(f, "web/templates/*.html"))
+	r.SetHTMLTemplate(t)
 	r.GET("/hello", hello.Hello)
 	r.Run()
 }
