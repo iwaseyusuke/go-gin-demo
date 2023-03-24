e1bfac1d37cc4d78759d56a439eae5616063c831
Yusuke Iwase <iwaseyusuke@users.noreply.github.com>
2023-03-24 15:53:42 +0900

    config: Use BP_KEEP_FILES instead of embed (#2)
    Signed-off-by: Yusuke Iwase <iwase.yusuke0@gmail.com>

--- main.go
+++ main.go
@@ -1,21 +1,14 @@
 package main
 
 import (
-	"embed"
-	"html/template"
-
 	"github.com/iwaseyusuke/go-gin-demo/pkg/hello"
 
 	"github.com/gin-gonic/gin"
 )
 
-//go:embed web/templates/*.html
-var f embed.FS
-
 func main() {
 	r := gin.Default()
-	t := template.Must(template.New("").ParseFS(f, "web/templates/*.html"))
-	r.SetHTMLTemplate(t)
+	r.LoadHTMLGlob("web/templates/*")
 	r.GET("/hello", hello.Hello)
 	r.Run()
 }
